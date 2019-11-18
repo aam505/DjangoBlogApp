76fc0a2b2e711fc2c74174964e20e7aad0e14a06
aam505 <andreea.muresan505@gmail.com>
2019-11-18 17:15:20 +0100

    Template django app.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -4,9 +4,27 @@ from django.http import HttpResponse
 
 # This is where you add the logic to how to handle certain routes 
  
+
+posts =[
+    {
+        'author':'Jane Doe',
+        'title':'First blog post',
+        'content':'First post content',
+        'date_posted':'August 27, 2019'
+    },
+    {
+        'author':'Jane Doe',
+        'title':'Second blog post',
+        'content':'Second post content',
+        'date_posted':'August 28, 2019'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request,'blog/about.html')
