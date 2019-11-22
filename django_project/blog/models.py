916a80967b1cd5f9bc4f82223c48ba813cf78ec0
aam505 <andreea.muresan505@gmail.com>
2019-11-22 16:42:38 +0100

    Add, delete, update

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -13,4 +14,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title;
+
+    #get absolute url method so django knows how to get to that post
+    #return url as string with reverse
     
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk':self.pk})
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin # mixins have to be to the left inheritence
+# Create your views here. 
 
 # This is where you add the logic to how to handle certain routes 
  
@@ -13,5 +14,52 @@ def home(request):
     return render(request,'blog/home.html', context)
 
  
+class PostListView(ListView):
+    model = Post
+    template_name='blog/home.html'
+    context_object_name='posts'
+    ordering=['-date_posted']
+    #by default itnames the posts variable above as object_list
+    #app/module_viewtype.html
+
+class PostDetailView(DetailView):
+    model = Post
+  
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url='/'
+    def test_func(self):
+        post=self.get_object()
+        if(self.request.user == post.author):
+            return True 
+        else:
+            return False
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields=['title', 'content']
+
+    #override form valid method
+    def form_valid(self, form):
+        form.instance.author = self.request.user # setting the author befor the parent form valid gets run
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields=['title', 'content']
+
+    #override form valid method
+    def form_valid(self, form):
+        form.instance.author = self.request.user # setting the author befor the parent form valid gets run
+        return super().form_valid(form)
+
+    #used by user passes test mixin
+    def test_func(self):
+        post=self.get_object()
+        if(self.request.user == post.author):
+            return True 
+        else:
+            return False    
+
 def about(request):
     return render(request,'blog/about.html',{'title':'DJANGO'})
