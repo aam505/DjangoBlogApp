c32894706e6f0b5bbed7121b1c818c0b2adefe32
aam505 <andreea.muresan505@gmail.com>
2019-11-25 14:30:21 +0100

    Filter posts by user

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin # mixins have to be to the left inheritence
 # Create your views here. 
@@ -19,9 +20,20 @@ class PostListView(ListView):
     template_name='blog/home.html'
     context_object_name='posts'
     ordering=['-date_posted']
-    paginate_by=2
+    paginate_by=5
     #by default itnames the posts variable above as object_list
     #app/module_viewtype.html
+
+class UserPostListView(ListView):
+    model = Post
+    template_name='blog/user_posts.html'
+    context_object_name='posts'
+    paginate_by=5
+
+    #to modify the query set that this list view returns we can override this method
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
